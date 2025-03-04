b276c64ce38120037f755765bbe8b6349f87cfa8
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-03-04 10:34:42 +0600

    Connection with HTMl

--- Session 3 [jinja , Template and Inheritance ]/app.py
+++ Session 3 [jinja , Template and Inheritance ]/app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask , render_template
 # Create the Flask App
 
-app = Flask(__name__)
+app = Flask(__name__ , )
 
 # Home page
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1> Welcome to the Home page ! </h1>"
+    return render_template("home.html")
 
 # About Page
 @app.route("/about")
 def about():
-    return "<h1> Welcome to the About page </h1>"
+    return render_template("about.html")
 
 
 
